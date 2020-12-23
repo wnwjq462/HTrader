64856ac1d83ea1d548cf941fd9da646dabb4257b
wnwjq462 <74763242+wnwjq462@users.noreply.github.com>
2020-12-23 20:43:36 +0900

    Update Kiwoom.py

--- Kiwoom.py
+++ Kiwoom.py
@@ -10,27 +10,31 @@ from datetime import datetime
 
 TR_REQ_TIME_INTERVAL = 0.2
 
-
+#키움 API기능을 이용하는 메인 클래스
 class Kiwoom(QAxWidget):
     def __init__(self):
         super().__init__()
         self._create_kiwoom_instance()
         self._set_signal_slots()
 
+    #키움 ProgID 연결
     def _create_kiwoom_instance(self):
         self.setControl("KHOPENAPI.KHOpenAPICtrl.1")
 
+    #이벤트 핸들러 연결
     def _set_signal_slots(self):
         self.OnEventConnect.connect(self._event_connect)
         self.OnReceiveTrData.connect(self._receive_tr_data)
         self.OnReceiveChejanData.connect(self._receive_chejan_data)
         self.OnReceiveRealData.connect(self._receive_real_data)
 
+    #API에 접속하는 함수
     def comm_connect(self):
         self.dynamicCall("CommConnect()")
         self.login_event_loop = QEventLoop()
         self.login_event_loop.exec_()
 
+    #접속 상태 표시
     def _event_connect(self, err_code):
         if err_code == 0:
             print("connected")
@@ -39,36 +43,44 @@ class Kiwoom(QAxWidget):
 
         self.login_event_loop.exit()
 
+    #KOSPI, KOSDAQ별 종목 코드 리스트 불러오는 함수
     def get_code_list_by_market(self, market):
         code_list = self.dynamicCall("GetCodeListByMarket(QString)", market)
         code_list = code_list.split(';')
         return code_list[:-1]
 
+    #종목 코드를 이용하여 종목 명 얻어내는 함수
     def get_master_code_name(self, code):
         code_name = self.dynamicCall("GetMasterCodeName(QString)", code)
         return code_name
 
+    #연결 상태 반환 함수
     def get_connect_state(self):
         ret = self.dynamicCall("GetConnectState()")
         return ret
 
+    #정보 요청 시 input 설정
     def set_input_value(self, id, value):
         self.dynamicCall("SetInputValue(QString, QString)", id, value)
 
+    #데이터 요청 함수
     def comm_rq_data(self, rqname, trcode, next, screen_no):
         self.dynamicCall("CommRqData(QString, QString, int, QString)", rqname, trcode, next, screen_no)
         self.tr_event_loop = QEventLoop()
         self.tr_event_loop.exec_()
 
+    #요청 데이터 얻어오는 함수
     def _comm_get_data(self, code, real_type, field_name, index, item_name):
         ret = self.dynamicCall("CommGetData(QString, QString, QString, int, QString)", code,
                                real_type, field_name, index, item_name)
         return ret.strip()
 
+    #가져올 데이터의 개수를 반환하는 함수
     def _get_repeat_cnt(self, trcode, rqname):
         ret = self.dynamicCall("GetRepeatCnt(QString, QString)", trcode, rqname)
         return ret
 
+    #Receive 이벤트 발생시에 처리하는 함수
     def _receive_tr_data(self, screen_no, rqname, trcode, record_name, next, unused1, unused2, unused3, unused4):
         if next == '2':
             self.remained_data = True
@@ -89,17 +101,21 @@ class Kiwoom(QAxWidget):
         except AttributeError:
             pass
 
+    #실시간 데이터 Receive 이벤트 처리 함수
     def _receive_real_data(self, jcode, rtype, rdata):
             self.real_print(jcode,8019)
 
+    #실시간 데이터 정보 등록
     def _set_real_reg(self, screen, codelist, fidlist, rtype):
         ret = self.dynamicCall("SetRealReg(QString,QString,QString,QString)", screen,codelist,fidlist,rtype)
         return ret
 
+    #실시간 데이터 정보 삭제
     def _set_real_remove(self, screen, delcode):
         ret = self.dynamicCall("SetRealRemove(QString, QString",screen, delcode)
         return ret
 
+    #실시간 데이터 반환
     def real_print(self,jcode,fid):
         real = self.dynamicCall("GetCommRealData(QString,Int)",jcode,fid)
         print(real)
@@ -129,6 +145,7 @@ class Kiwoom(QAxWidget):
         con.commit()
         con.close()
     '''
+    #종목의 일봉 데이터 가져오는 함수
     def _opt10081(self, rqname, trcode):
         data_cnt = self._get_repeat_cnt(trcode, rqname)
         print(data_cnt)
@@ -157,13 +174,16 @@ class Kiwoom(QAxWidget):
             self.ohlcv['close'].append(int(close))
             self.ohlcv['volume'].append(int(volume))
 
+    #주문 요청
     def send_order(self, rqname, screen_no, acc_no, order_type, code, quantity, price, hoga, order_no):
         self.dynamicCall("SendOrder(QString, QString, QString, int, QString, int, int, QString, QString", [rqname, screen_no, acc_no, order_type,code, quantity, price, hoga, order_no])
 
+    #fid에 따른 체결잔고 데이터를 가져오는 함수
     def get_chejan_data(self,fid):
         ret = self.dynamicCall("GetChejanData(int)", fid)
         return ret
 
+    #체결잔고 데이터 출력
     def _receive_chejan_data(self, gubun, item_cnt, fid_list):
         print(gubun)
         print(self.get_chejan_data(9203))
@@ -171,10 +191,12 @@ class Kiwoom(QAxWidget):
         print(self.get_chejan_data(900))
         print(self.get_chejan_data(901))
 
+    #로그인 정보 반환
     def get_login_info(self, tag):
         ret = self.dynamicCall("GetLoginInfo(QString)", tag)
         return ret
 
+    #입력된 문자열 왼쪽의 '-','0'을 제거하고 천의 자리마다 콤마 추가한 문자열로 변경
     @staticmethod
     def change_format(data):
         strip_data = data.lstrip('-0')
@@ -187,6 +209,7 @@ class Kiwoom(QAxWidget):
 
         return format_data
 
+    #수익률에 대한 포맷 변경
     @staticmethod
     def change_format2(data):
         strip_data = data.lstrip('-0')
@@ -202,17 +225,21 @@ class Kiwoom(QAxWidget):
 
         return strip_data
 
+    #데이터를 가져오기 위한 딕셔너리 초기화
     def reset_opw00018_output(self):
         self.opw00018_output = {'single' : [], 'multi' : []}
 
+    #실제 서버인지 모의투자 서버인지 구분
     def get_server_gubun(self):
         ret = self.dynamicCall("KOA_Functions(QString, QString)", "GetServerGubun", "")
         return ret
 
+    #d+2 추정 예수금의 데이터를 가져오는 함수
     def _opw00001(self, rqname, trcode):
         d2_deposit = self._comm_get_data(trcode, "",rqname,0, "d+2추정예수금")
         self.d2_deposit = Kiwoom.change_format(d2_deposit)
 
+    #데이터를 가져오기 위한 딕셔너리 초기화
     def _opw00018(self, rqname, trcode):
 
         #single data
